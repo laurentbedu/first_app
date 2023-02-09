e6ed50632a01f5e8406d1a00c7ea2ee53dd7fdd9
laurentbedu <bedulaurent@gmail.com>
2023-02-09 16:15:11 +0100

    E03

--- src/screens/TodolistScreen/TodoListScreen.jsx
+++ src/screens/TodolistScreen/TodoListScreen.jsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
+
 function TodoListScreen() {
+
+    const [todoList, setTodoList] = useState([]);
+
+    useEffect(()=>{
+        const fecthData = async () => {
+            const resp = await fetch("http://localhost:5000/todo");
+            const todoListData = await resp.json();
+            setTodoList(todoListData.data);
+        }
+        fecthData();
+    }, [])
+
     return ( <>
         Todos List
+        <div>
+        {todoList.map(todo => {
+            return <div>{todo.title}</div>
+            //return console.log(todo)
+
+        })}
+        </div>
     </> );
 }
 
